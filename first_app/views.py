8c03f29f04b4e1e754c7e8638a714acb0160db46
Adarsh Kumar <adarshkumar9820@gmail.com>
2019-08-09 14:48:00 +0800

    django 2

--- first_app/views.py
+++ first_app/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
+
+from first_app.models import Topic, Webpage, AccessRecord
 # Create your views here.
 
 def index(request):
-    my_dict = {'insert_me': "Hello i am from views.py"}
-    return render(request, 'first_app/index.html', context=my_dict)
+    webpages = AccessRecord.objects.order_by('date')
+
+    # access_records is mentioned in html file
+    date_dict = {'access_records': webpages}
+    return render(request, 'first_app/index.html', context=date_dict)
 
 def help(request):
     my_dict = {'insert_me': "hello, this is help page"}
